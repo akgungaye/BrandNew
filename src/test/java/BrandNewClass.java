d6a2d5130f044e00e20085a72b9cb780fad171b4
gaye <gungorgaye@gmail.com>
2020-10-16 13:07:56 +0300

    again commit and choose unversioned files either

--- src/test/java/BrandNewClass.java
+++ src/test/java/BrandNewClass.java
@@ -2,6 +2,7 @@ public class BrandNewClass {
     public static void main(String[] args) {
         //This project is for github practice
         //make initial commit
+        //again commit
 
     }
 
