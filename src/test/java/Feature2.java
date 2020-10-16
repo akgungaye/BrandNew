7260496df157b5ade570dd838cf680d0ba2b7906
gaye <gungorgaye@gmail.com>
2020-10-16 13:46:14 +0300

    again commit

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -3,5 +3,6 @@ public class Feature2 {
         System.out.println("working on feature2");
         System.out.println("working on more feature2");
         System.out.println("working on feature2 again");
+        System.out.println("I forgot to commit");
     }
 }
