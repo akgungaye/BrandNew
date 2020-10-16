ed1143f30786b38322d22198501adc6cb05015b5
gaye <gungorgaye@gmail.com>
2020-10-16 13:45:03 +0300

    feature 2 added

---
+++ src/test/java/Feature2.java
@@ -0,0 +1,7 @@
+public class Feature2 {
+    public static void main(String[] args) {
+        System.out.println("working on feature2");
+        System.out.println("working on more feature2");
+        System.out.println("working on feature2 again");
+    }
+}
